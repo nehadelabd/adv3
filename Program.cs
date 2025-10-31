76986038aa3a85489dbc031a081adeecc296a727
nehad <Dark@DESKTOP-TQDVAO4>
2025-10-31 09:51:41 -0700

    12

--- Program.cs
+++ Program.cs
@@ -347,33 +347,60 @@ namespace adv3
             //};
             #endregion
             #region 13
-            SortedList<int, int> numbers = new SortedList<int, int>()
+        //    SortedList<int, int> numbers = new SortedList<int, int>()
+        //{
+        //    { 1, 5 },
+        //    { 2, 8 },
+        //    { 3, 10 },
+        //    { 4, 7 },
+        //    { 5, 12 }
+        //};
+
+        //    Console.WriteLine("Original SortedList (Key -> Value):");
+        //    foreach (var pair in numbers)
+        //        Console.WriteLine($"{pair.Key} -> {pair.Value}");
+
+        //    List<int> evenValueKeys = new List<int>();
+
+        //    foreach (var pair in numbers)
+        //    {
+        //        if (pair.Value % 2 == 0)
+        //            evenValueKeys.Add(pair.Key);
+        //    }
+
+        //    Console.WriteLine("\nKeys with even values:");
+        //    if (evenValueKeys.Count > 0)
+        //        Console.WriteLine(string.Join(", ", evenValueKeys));
+        //    else
+        //        Console.WriteLine("No even values found.");
+        //}
+        #endregion
+        #region 12
+                SortedSet<int> numbers = new SortedSet<int> { 2, 5, 8, 10, 15, 20 };
+
+                Console.WriteLine("SortedSet elements: " + string.Join(", ", numbers));
+
+                Console.Write("Enter a target number: ");
+                int target = int.Parse(Console.ReadLine());
+
+                List<int> greaterThanTarget = new List<int>();
+
+                foreach (int num in numbers)
+                {
+                    if (num > target)
+                        greaterThanTarget.Add(num);
+                }
+
+                if (greaterThanTarget.Count > 0)
+                {
+                    Console.WriteLine("\nElements greater than target:");
+                    Console.WriteLine(string.Join(", ", greaterThanTarget));
+                }
+                else
         {
-            { 1, 5 },
-            { 2, 8 },
-            { 3, 10 },
-            { 4, 7 },
-            { 5, 12 }
-        };
-
-            Console.WriteLine("Original SortedList (Key -> Value):");
-            foreach (var pair in numbers)
-                Console.WriteLine($"{pair.Key} -> {pair.Value}");
-
-            List<int> evenValueKeys = new List<int>();
-
-            foreach (var pair in numbers)
-            {
-                if (pair.Value % 2 == 0)
-                    evenValueKeys.Add(pair.Key);
-            }
-
-            Console.WriteLine("\nKeys with even values:");
-            if (evenValueKeys.Count > 0)
-                Console.WriteLine(string.Join(", ", evenValueKeys));
-            else
-                Console.WriteLine("No even values found.");
+            Console.WriteLine("\nNo elements greater than target were found.");
         }
+            }
         #endregion
 
     }

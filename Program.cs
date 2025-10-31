7d928e97a9e835e5a26b2f3be18ea8e2edcaf592
nehad <Dark@DESKTOP-TQDVAO4>
2025-10-31 05:07:55 -0700

    1

--- Program.cs
+++ Program.cs
@@ -3,26 +3,26 @@
     internal class Program
     {
         #region 1
-        static int FirstNonRepeatedCharIndex(string s)
-        {
-            Dictionary<char, int> frequency = new Dictionary<char, int>();
+        //static int FirstNonRepeatedCharIndex(string s)
+        //{
+        //    Dictionary<char, int> frequency = new Dictionary<char, int>();
 
-            foreach (char c in s)
-            {
-                if (frequency.ContainsKey(c))
-                    frequency[c]++;
-                else
-                    frequency[c] = 1;
-            }
+        //    foreach (char c in s)
+        //    {
+        //        if (frequency.ContainsKey(c))
+        //            frequency[c]++;
+        //        else
+        //            frequency[c] = 1;
+        //    }
 
-            for (int i = 0; i < s.Length; i++)
-            {
-                if (frequency[s[i]] == 1)
-                    return i;
-            }
+        //    for (int i = 0; i < s.Length; i++)
+        //    {
+        //        if (frequency[s[i]] == 1)
+        //            return i;
+        //    }
 
-            return -1;
-        } 
+        //    return -1;
+        //} 
         #endregion
 
         static void Main(string[] args)
@@ -32,15 +32,15 @@
 
 
             #region 1
-            Console.Write("Enter a string: ");
-            string input = Console.ReadLine();
+            //Console.Write("Enter a string: ");
+            //string input = Console.ReadLine();
 
-            int index = FirstNonRepeatedCharIndex(input);
+            //int index = FirstNonRepeatedCharIndex(input);
 
-            if (index != -1)
-                Console.WriteLine($"First non-repeated character is '{input[index]}' at index {index}");
-            else
-                Console.WriteLine("No non-repeated character found.");
+            //if (index != -1)
+            //    Console.WriteLine($"First non-repeated character is '{input[index]}' at index {index}");
+            //else
+            //    Console.WriteLine("No non-repeated character found.");
             #endregion
         }
     }

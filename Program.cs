1aa9494d7858fea4b52f1d7d0b71d959922b34a1
nehad <Dark@DESKTOP-TQDVAO4>
2025-10-31 05:09:39 -0700

    2

--- Program.cs
+++ Program.cs
@@ -25,6 +25,27 @@
         //} 
         #endregion
 
+        #region 2
+
+        //static void CountFrequencies(int[] arr)
+        //{
+        //    Dictionary<int, int> frequency = new Dictionary<int, int>();
+
+        //    foreach (int num in arr)
+        //    {
+        //        if (frequency.ContainsKey(num))
+        //            frequency[num]++;
+        //        else
+        //            frequency[num] = 1;
+        //    }
+
+        //    Console.WriteLine("Element : Frequency");
+        //    foreach (var pair in frequency)
+        //    {
+        //        Console.WriteLine($"{pair.Key} : {pair.Value}");
+        //    }
+        //}
+        #endregion
         static void Main(string[] args)
         {
 
@@ -41,6 +62,14 @@
             //    Console.WriteLine($"First non-repeated character is '{input[index]}' at index {index}");
             //else
             //    Console.WriteLine("No non-repeated character found.");
+            #endregion
+            #region 2
+
+
+
+            //int[] arr = { 1, 2, 2, 3, 1, 4, 2 };
+
+            //CountFrequencies(arr);
             #endregion
         }
     }
